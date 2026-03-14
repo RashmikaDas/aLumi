523922a5b465d14c8caa60b565527843991de376
Rashmika Das <rashmikadas02@gmail.com>
2026-03-14 21:37:27 +0530

    Initial Commit

---
+++ aLumi/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  aLumi
+//
+//  Created by admin17 on 14/03/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ aLumi/aLumiApp.swift
@@ -0,0 +1,17 @@
+//
+//  aLumiApp.swift
+//  aLumi
+//
+//  Created by admin17 on 14/03/26.
+//
+
+import SwiftUI
+
+@main
+struct aLumiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
